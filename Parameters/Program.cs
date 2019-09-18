354d8a325a4801913edd520ca6fd37f0dc3cc1fa
Joaquim Barreto <joaquim@barreto.co>
2019-09-18 11:22:22 +0200

    Done

--- Parameters/Program.cs
+++ Parameters/Program.cs
@@ -12,7 +12,14 @@ namespace Parameters
     {
         static void doWork()
         {
-            // TODO:
+            int i = 0;
+            Console.WriteLine(i);
+            Pass.Value(ref i);
+            Console.WriteLine(i);
+            WrappedInt wi = new WrappedInt();
+            Console.WriteLine(wi.Number);
+            Pass.Reference(wi);
+            Console.WriteLine(wi.Number);
         }
 
         static void Main(string[] args)
